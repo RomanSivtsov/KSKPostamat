4fa3f3b7763f5e5786e90506549c1b54030ac394
RomanSivtsov <tvgustav@gmail.com>
2026-06-10 11:48:11 +0300

    added models

---
+++ Core/Models/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Cell
+    {
+        public int CellId { get; set; }
+        public int? OrderId { get; set; }
+        public bool OrderIsPaid { get; set; }
+        public bool CellHasPackage { get; set; }
+        public int isClosed { get; set; }
+        public bool isTaken { get; set; }
+    }
+}
---
+++ Core/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsPaid { get; set; }
+        public int CellId { get; set; }
+    }
+}
